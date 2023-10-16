8c7da5f792e5c265a716612181a341c60862efce
Maheshwaran Suresh <msur0008@student.monash.edu>
2023-10-16 14:13:05 +1100

    WU-G2: edit

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,3 +3,8 @@ vegaEmbed('#box_plot', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var spec1 = "js/box_plot_chart.json";
+vegaEmbed('#box_plot2', spec1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
