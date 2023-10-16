29f0aadeebf24485ae17356888f48264b2ad271e
Maheshwaran Suresh <msur0008@student.monash.edu>
2023-10-16 11:12:01 +1100

    added boxplot

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var spec2 = "js/daily_cost_bar_chart.json";
-vegaEmbed('#bar_chart', spec2).then(function(result) {
+var spec1 = "js/box_plot_chart.json";
+vegaEmbed('#box_plot', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
