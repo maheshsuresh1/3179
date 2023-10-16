c8ca02ee2f888c337bc59491be09b52782ffd238
Maheshwaran Suresh <msur0008@student.monash.edu>
2023-10-16 17:14:02 +1100

    t2

--- js/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var spec1 = "js/bar_chart.vg.json";
-vegaEmbed('#bar_chart', spec1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
