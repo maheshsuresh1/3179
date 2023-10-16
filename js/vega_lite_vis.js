f9d8a9a6a74ed13732df130c84fcfaf63f621e30
Maheshwaran Suresh <msur0008@student.monash.edu>
2023-10-16 17:05:14 +1100

    t1

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,10 +1,4 @@
-var spec1 = "js/box_plot_chart.json";
-vegaEmbed('#box_plot', spec1).then(function(result) {
+var spec1 = "js/bar_chart.vg.json";
+vegaEmbed('#bar_chart', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-
-var spec1 = "js/box_plot_chart.json";
-vegaEmbed('#box_plot2', spec1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
